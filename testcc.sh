4e65326e209394be6c080981a1fc89e3fbdc8b91
Ratnakar <asara.ratnakar@gmail.com>
2017-05-05 23:43:47 -0400

    Update testcc.sh

--- testcc.sh
+++ testcc.sh
@@ -4,7 +4,7 @@ set -e
 
 printf "\n\n********************** START ***********************\n\n"
 printf "Installing chaincode example02\n\n"
-peer chaincode install -o 127.0.0.1:7050 -n mycc -v 1.0 -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02
+peer chaincode install -n mycc -v 1.0 -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02
 printf "\n\nInstantiate chaincode example02\n\n"
 peer chaincode instantiate -o 127.0.0.1:7050 -n mycc -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}'
 
